8cfa3074b44ea22a736542f808bb11e74eec75d3
Matias <mati.nicolosi@gmail.com>
2020-04-20 19:42:12 -0300

    Avance en la implementación de "recuperar".

--- src/main/java/ar/edu/unq/eperdemic/persistencia/dao/jdbc/JDBCPatogenoDAO.kt
+++ src/main/java/ar/edu/unq/eperdemic/persistencia/dao/jdbc/JDBCPatogenoDAO.kt
@@ -3,6 +3,7 @@ package ar.edu.unq.eperdemic.persistencia.dao.jdbc
 import ar.edu.unq.eperdemic.modelo.Patogeno
 import ar.edu.unq.eperdemic.persistencia.dao.PatogenoDAO
 import ar.edu.unq.eperdemic.persistencia.dao.jdbc.JDBCConnector.execute
+import java.sql.Connection
 
 
 class JDBCPatogenoDAO : PatogenoDAO {
@@ -16,7 +17,23 @@ class JDBCPatogenoDAO : PatogenoDAO {
     }
 
     override fun recuperar(patogenoId: Int): Patogeno {
-        TODO("not implemented")
+        return execute { conn: Connection ->
+            val ps = conn.prepareStatement("SELECT tipo, cantidadDeEspecies FROM patogeno WHERE id = patogenoId")
+            ps.setString(1, id)
+            val resultSet = ps.executeQuery()
+            var patogeno: Patogeno? = null
+            while (resultSet.next()) {
+                //si patogeno no es null aca significa que el while dio mas de una vuelta, eso
+                //suele pasar cuando el resultado (resultset) tiene mas de un elemento.
+                if (patogeno != null) {
+                    throw RuntimeException("Existe mas de un patogeno con el id $id")
+                }
+                patogeno = Patogeno(tipo)
+                patogeno.cantidadDeEspecies = resultSet.getInt("cantidadDeEspecies")
+            }
+            ps.close()
+            patogeno!!
+        }
     }
 
     override fun recuperarATodos(): List<Patogeno> {
